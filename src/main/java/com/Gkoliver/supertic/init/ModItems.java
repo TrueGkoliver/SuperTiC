4b0a0e7359546063905ae9a6b2f2d4f5393cf891
TrueGkoliver <gkoliver05@gmail.com>
2018-09-22 12:30:32 -0500

    Fixed the Western Charm
    Took long enough

--- src/main/java/com/Gkoliver/supertic/init/ModItems.java
+++ src/main/java/com/Gkoliver/supertic/init/ModItems.java
@@ -1,21 +1,25 @@
 package com.Gkoliver.supertic.init;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.Gkoliver.supertic.items.ItemBase;
 import com.Gkoliver.supertic.items.ItemByzantineTrident;
 import com.Gkoliver.supertic.items.byzantine_material.ByzantineSword;
+import com.Gkoliver.supertic.items.byzantine_material.WestCharm;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -45,6 +49,49 @@ public class ModItems
 	    }
 
 	};
-	
+	//stub
+	public static final Item WEST_CHARM = new WestCharm("charm_western") {
+		private NonNullList<ItemStack> invarray;
+		private Iterator<ItemStack> oi;
+		private ItemStack thing;
+		private Iterator<ItemStack> mmm;
+		private Iterator<ItemStack> lll;
+
+		public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+	    {
+			World the_world = worldIn;
+			EntityPlayer area = playerIn;
+			InventoryPlayer inv = area.inventory;
+			invarray = inv.mainInventory;
+			oi = invarray.iterator();
+			mmm = inv.armorInventory.iterator();
+			
+			if (playerIn.getHeldItemOffhand() != null) {
+				lll = inv.offHandInventory.iterator();
+				while (lll.hasNext()) {
+					thing = lll.next();
+					if (thing.isItemDamaged()) {
+						thing.setItemDamage(0);
+					}
+				} 
+			}
+			
+			
+			while (oi.hasNext()) {
+				thing = oi.next();
+				if (thing.isItemDamaged()) {
+					thing.setItemDamage(0);
+				}
+			}
+			while (mmm.hasNext()) {
+				thing = mmm.next();
+				if (thing.isItemDamaged()) {
+					thing.setItemDamage(0);
+				}
+			}
+	        return new ActionResult<ItemStack>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
+	    }
+
+	};
 	
 }
---
+++ src/main/java/com/Gkoliver/supertic/items/byzantine_material/WestCharm.java
@@ -0,0 +1,12 @@
+package com.Gkoliver.supertic.items.byzantine_material;
+
+import com.Gkoliver.supertic.items.ItemBase;
+
+public class WestCharm extends ItemBase {
+
+	public WestCharm(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
